51b4248b398cf6bbb8792bfca7f7e7347fc03945
Vernalhav <vernalhav@usp.br>
2019-08-05 22:40:12 -0300

    Refactor with a new Scraper class

--- wallpaperGui.py
+++ wallpaperGui.py
@@ -1,14 +1,12 @@
 import sys
 import os
 import random
-import re
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-from requests import get
-from bs4 import BeautifulSoup
+from Scraper import Scraper
 
 '''
     Code that displays Windows Screensaver images and downloads
@@ -22,17 +20,13 @@ class WallpaperGUI(QDialog):
 
     def __init__(self):
         super().__init__()
-        self.TempFolderName = "temp"
         self.title = "Wallpaper Downloader"
         self.left = 0
         self.top = 0
         self.width = 900
         self.height = 600
 
-        self.downloadPath = ""
-        self.lastImageName = None
-
-        self.siteDomain = "http://www.bingwallpaperhd.com"
+        self.scraper = Scraper()
 
         # Labels for the image categories of bingwallpaperhd.com
         self.labels = {
@@ -44,11 +38,6 @@ class WallpaperGUI(QDialog):
             "Space"  : ["Space"],
             "Other"  : ["Other"]
         }
-
-        if not os.path.exists(self.TempFolderName):
-            os.mkdir(self.TempFolderName)
-        else:
-            self.clearTempFolder()
 
         self.initUI()
 
@@ -81,8 +70,6 @@ class WallpaperGUI(QDialog):
         self.searchBtn.clicked.connect(self.search)
         self.downloadBtn.clicked.connect(self.download)
 
-        self.mainLayout = QGridLayout()
-
         self.mainLayout.addLayout(self.topHorizontalLayout, 0, 0)
         self.mainLayout.addWidget(self.mainImageView, 1, 0)
         self.mainLayout.addLayout(self.bottomHorizontalLayout, 2, 0)
@@ -101,34 +88,13 @@ class WallpaperGUI(QDialog):
         self.show()
 
     def closeEvent(self, event):
-        self.deletePreviousImage()
-        self.clearTempFolder()
-        os.rmdir(self.TempFolderName)
+        self.scraper.deletePreviousImage()
+        self.scraper.clearTempFolder()
+        self.scraper.deleteTempFolder()
         event.accept()
-
-    def download(self):
-        if (self.lastImageName == None):
-            self.showMessage("No image selected")
-            return
-
-        if (self.downloadPath == ""):
-            self.selectNewDirectory()
-
-        # User cancelled directory select, so don't download the image
-        if (self.downloadPath == ""):
-            print("Download cancelled")
-            return
-
-        if (os.path.isfile( os.path.join(self.TempFolderName, self.lastImageName ))):
-            os.rename( os.path.join(self.TempFolderName, self.lastImageName), os.path.join(self.downloadPath, self.lastImageName) )
 
     def showMessage(self, msg):
         QMessageBox.information(self, "Wallpaper Downloader", msg)
-
-    def selectNewDirectory(self):
-        dirPath = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-
-        self.downloadPath = dirPath
 
     def setUpDropdowns(self):
         self.mainStyleDropdown.addItem("Nature")
@@ -154,74 +120,41 @@ class WallpaperGUI(QDialog):
             self.subStyleDropdown.addItem(label)
 
     def updateGraphicsView(self):
-        self.pixmap = QPixmap( os.path.join(self.TempFolderName, self.lastImageName) )
+        self.pixmap = QPixmap( self.scraper.getImagePath() )
         self.mainScene.addPixmap(self.pixmap)
         self.mainImageView.show()
         self.mainImageView.fitInView(self.mainScene.sceneRect(), Qt.KeepAspectRatio)
 
     def search(self):
-        self.deletePreviousImage()
+        self.scraper.deletePreviousImage()
 
         mainCategory = self.mainStyleDropdown.currentText().lower()
         subCategory = self.subStyleDropdown.currentText().lower()
 
-        url = self.siteDomain + "/" + mainCategory + ["/" + subCategory, ""][mainCategory == subCategory]
-        response = get(url)
-        if (response.status_code != 200):
-            self.showMessage("Error loading page")
-            return
+        self.scraper.search(mainCategory, subCategory)
 
-        maxPages = self.getMaxPages(response)
-        randomPage = random.randint(1, maxPages)
-        imagePageUrl = url + "/page/" + str(randomPage)     # gets random image page URL
-
-        response = get(imagePageUrl)
-        if (response.status_code != 200):
-            self.showMessage("Error loading page")
-            return
-
-        imageUrl = self.getRandomImageURL(response)
-        imageName = re.sub(r".*/", "", imageUrl)   # removes the rest of the url
-
-        with open(os.path.join(self.TempFolderName, imageName), "wb") as f:
-            f.write(get(imageUrl).content)
-            self.lastImageName = imageName
-            self.updateGraphicsView()
-
-    def deletePreviousImage(self):
-        if (self.lastImageName == None):
-            return
-
-        if (os.path.isfile( os.path.join(self.TempFolderName, self.lastImageName ))):
-            os.remove(os.path.join(self.TempFolderName, self.lastImageName))
-
-    def getRandomImageURL(self, response):
-        ''' Returns the URL of a random image in the page '''
-        soup = BeautifulSoup(response.text, "html.parser")
-        container = soup.find_all("div", class_ = "grid_8 post")[0]
-        imageDivs = container.find_all("div", class_ = "view view-first")
-
-        imageNumber = random.randint(0, len(imageDivs) - 1)
-        smallImageLink = imageDivs[imageNumber].a.img.attrs["src"]
-
-        # removes the dimensions of the small icon, linking to the original image
-        return re.sub(r"-\d*x\d*\.", ".", smallImageLink)
-
-    def getMaxPages(self, response):
-        ''' Returns the number of pages with results from the specified categories '''
-        soup = BeautifulSoup(response.text, "html.parser")
-        container = soup.find_all("div", class_ = "wp-pagenavi")[0]    # since there is only one page nav, we get the first (and only) result
-
-        maxPages = int(container.find_all("a")[-2].text)
-        return maxPages
+        self.updateGraphicsView()
 
     def randomizeSearch(self):
         self.mainStyleDropdown.setCurrentIndex( random.randint(0, len(self.labels) - 1) )
-
         currentCategory = self.mainStyleDropdown.currentText()
-
         self.subStyleDropdown.setCurrentIndex( random.randint(0, len(self.labels[currentCategory]) - 1) )
+
         self.search()
+
+    def download(self):
+        status = self.scraper.download()
+
+        if (status == "No directory chosen"):
+            self.selectNewDirectory()
+            self.scraper.download()
+
+        elif (status != "Success"):
+            self.showMessage(status)
+
+    def selectNewDirectory(self):
+        dirPath = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.scraper.setNewDirectory(dirPath)
 
     def center(self):
         frameGm = self.frameGeometry()
@@ -230,15 +163,39 @@ class WallpaperGUI(QDialog):
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
 
-    def clearTempFolder(self):
-        for fileName in os.listdir(os.path.join(self.TempFolderName)):
-            fileName = os.path.join(self.TempFolderName, fileName)
-            if (os.path.isfile(fileName)):
-                os.remove(fileName)
-            elif (os.path.isdir(fileName)):
-                os.rmdir(fileName)
-            else:
-                print("Please delete contents of temp folder")
+# Still not being used. Plan to do so in the near future
+class ProgressBarWindow(QDialog):
+
+    def __init__(self):
+        QDialog.__init__(self)
+
+        self.title = "Searching..."
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 100
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(0, 0, self.width, self.height)
+
+        self.progressBar = QProgressBar()
+        self.progressBar.setValue(100)
+
+        self.mainLayout = QHBoxLayout()
+        self.mainLayout.addWidget(self.progressBar)
+        self.setLayout(self.mainLayout)
+
+        self.center()
+
+    def center(self):
+        frameGm = self.frameGeometry()
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        centerPoint = QApplication.desktop().screenGeometry(screen).center()
+        frameGm.moveCenter(centerPoint)
+        self.move(frameGm.topLeft())
 
 
 def main():
